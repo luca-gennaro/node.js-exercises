fc8775746d1e02dbd91cac3a7f597cb7abd59039
Luca Gennaro <lucagennaro91@gmail.com>
2023-01-11 12:41:26 +0100

    exercise-17 done

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
-import express from "express"
+import express, { response } from "express"
 import {PrismaClient} from "@prisma/client"
 
 const app = express()
 const prisma = new PrismaClient()
+app.use(express.json())
 
 app.get("/planets", async (request, response) => {
     const planets = await prisma.planet.findMany()
     response.json(planets)
+})
+app.post("/planets", async (request, response) => {
+    const planet = await request.body
+    response.json(planet)
 })
 
 app.listen(3000, ()=> console.log("running on port",3000 ))
